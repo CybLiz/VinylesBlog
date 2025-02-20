fb0a0956e596e1ecbb133540d72d0484d886dbd9
unknown <liza.bahloul@gmail.com>
2025-02-20 13:51:28 +0100

    TP updated

--- resources/views/vinyle.blade.php
+++ resources/views/vinyle.blade.php
@@ -4,24 +4,31 @@
     </x-slot:heading>
     <div class="flex items-center space-x-6 p-6 bg-white rounded-md shadow-md">
         <img src="{{ $vinyle['img'] }}" class="w-48 h-48 object-cover rounded-md" alt="{{ $vinyle['title'] }}">
-    
+
         <div class="flex flex-col space-y-2">
             <h3 class="text-xl font-semibold">{{ $vinyle['title'] }}</h3>
-            <p class="text-lg text-gray-600">{{ $vinyle['artist'] }}</p>
-            <p class="text-gray-500">{{ $vinyle['country'] }}</p>
+            <p class="text-lg text-gray-600">{{ $vinyle->artist->name }}</p>
+            <p class="text-gray-500">{{ $vinyle->artist->name }}</p>
             <p class="text-gray-400">{{ $vinyle['birth_year'] }}</p>
+            {{-- {{dd($comments)}} --}}
+            <h1>Description :</h1>
             <p class="text-gray-700">{{ $vinyle['description'] }}</p>
+          
         </div>
 
         <div>
-            {{-- {{dd($comments)}} --}}
-            @foreach($vinyle->comment as $comment)
-            <p class="text-gray-700">{{ $comment->content }}</p>
-            <p class="text-gray-700"><b>{{ $comment->user->name }}</b></p>
 
+            <h1>Comments :</h1>
+
+            @foreach ($vinyle->comment as $comment)
+                <p class="text-gray-700">{{ $comment->content }}</p>
+                <p class="text-gray-700"><b>{{ $comment->user->name }}</b></p>
             @endforeach
+        
+
+
         </div>
     </div>
-    
+
 
 </x-layout>
